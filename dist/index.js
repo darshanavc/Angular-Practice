491584f12315a25d37379521d3e2e360f85b0769
Darshana Vadalia <jaydip.vala@viitor.cloud>
2022-06-02 16:50:13 +0530

    update code

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,20 @@
 "use strict";
+function addition(a, b) {
+    return a + b;
+}
+console.log("addition :", addition(2, 4));
+function subtraction(a, b) {
+    return a - b;
+}
+console.log("subtraction :", subtraction(12, 4));
+function multiplication(a, b) {
+    return a * b;
+}
+console.log("multiplication :", multiplication(12, 3));
+function division(a, b) {
+    return a / b;
+}
+console.log("division  :", division(20, 5));
 function math(opp, a, b) {
     if (opp == "add") {
         return a + b;
@@ -17,3 +33,28 @@ function math(opp, a, b) {
     }
 }
 console.log(math("div", 4, 5));
+const arr1 = [1, 2, 3, 4, 5, 6];
+let temp = "";
+for (let i = 0; i < arr1.length; i++) {
+    temp += arr1[i] + " ";
+}
+console.log(temp);
+function myFunction() {
+    let text;
+    let favDrink = prompt("What's your favorite drink?", "Coca-Cola");
+    switch (favDrink) {
+        case "Coca-Cola":
+            text = "Excellent choice. Coca-Cola is good for your soul.";
+            break;
+        case "Pepsi":
+            text = "Pepsi is my favorite too!";
+            break;
+        case "Sprite":
+            text = "Really? Are you sure the Sprite is your favorite?";
+            break;
+        default:
+            text = "I have never heard of that one..";
+    }
+    console.log(text);
+}
+myFunction();
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
 
 
-// function addition(a:number, b:number){
-//     return a + b;     
-// }
-// console.log("addition :" , addition(2,4));
+function addition(a:number, b:number){
+    return a + b;     
+}
+console.log("addition :" , addition(2,4));
 
-// function subtraction(a:number, b:number){
-//     return a - b;     
-// }
-// console.log("subtraction :" , subtraction(12,4));
+function subtraction(a:number, b:number){
+    return a - b;     
+}
+console.log("subtraction :" , subtraction(12,4));
 
-// function multiplication(a:number, b:number){
-//     return a * b;     
-// }
-// console.log("multiplication :" , multiplication(12,3));
+function multiplication(a:number, b:number){
+    return a * b;     
+}
+console.log("multiplication :" , multiplication(12,3));
 
-// function division (a:number, b:number){
-//     return a / b;     
-// }
-// console.log("division  :" , division (20,5));
+function division (a:number, b:number){
+    return a / b;     
+}
+console.log("division  :" , division (20,5));
 
 function math(opp:string,a:number, b:number){
     if (opp == "add"){
@@ -39,6 +39,21 @@ function math(opp:string,a:number, b:number){
    
 }
 console.log(math("div", 4, 5));
+
+const arr1:number[] = [1,2,3,4,5,6];
+
+let temp = "";
+for (let i = 0; i < arr1.length; i++) {
+  temp += arr1[i] + " ";
+}
+
+// for (let i = 0; i < 3; i++) {
+//     temp += arr1[i] + " ";
+//   }
+console.log(temp);
+
+
+
 
 
 
